d31f698e600b4e14ded285815a928973751a5299
Gastech99 <diabygassamad16@gmail.com>
2024-07-01 20:38:51 +0000

    Création de l'entité PRODUIT et de son interface REPOSITORY

---
+++ src/main/java/com/udemy/ecommerce/entities/PRODUIT.java
@@ -0,0 +1,62 @@
+package com.udemy.ecommerce.entities;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class PRODUIT {
+	private Long idProduit;
+	private String nomProduit;
+	private Double prixProduit;
+	private Date dateCreation;
+	
+	
+	public PRODUIT() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public PRODUIT(Long idProduit, String nomProduit, Double prixProduit, Date dateCreation) {
+		super();
+		this.nomProduit = nomProduit;
+		this.prixProduit = prixProduit;
+		this.dateCreation = dateCreation;
+	}
+
+
+	public Long getIdProduit() {
+		return idProduit;
+	}
+	public void setIdProduit(Long idProduit) {
+		this.idProduit = idProduit;
+	}
+	public String getNomProduit() {
+		return nomProduit;
+	}
+	public void setNomProduit(String nomProduit) {
+		this.nomProduit = nomProduit;
+	}
+	public Double getPrixProduit() {
+		return prixProduit;
+	}
+	public void setPrixProduit(Double prixProduit) {
+		this.prixProduit = prixProduit;
+	}
+	public Date getDateCreation() {
+		return dateCreation;
+	}
+	public void setDateCreation(Date dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+
+
+	@Override
+	public String toString() {
+		return "PRODUIT [idProduit=" + idProduit + ", nomProduit=" + nomProduit + ", prixProduit=" + prixProduit
+				+ ", dateCreation=" + dateCreation + "]";
+	}
+	
+	
+}
